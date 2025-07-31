fced37dc62f6dd437140a96da3f37108996ab528
Mirsadc1971 <mirsadc1971@icloud.com>
2025-07-31 18:04:00 -0500

    Update Bellwood.tsx

--- src/pages/areas/Bellwood.tsx
+++ src/pages/areas/Bellwood.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 const Bellwood: React.FC = () => {
   useEffect(() => {
